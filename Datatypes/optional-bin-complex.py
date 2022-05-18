3e8b872a107e49e45a9bb302932f14c98d8e5ecd
DianaBeki <dianabeki54@gmail.com>
2022-05-18 08:31:19 +0300

    complex

---
+++ Datatypes/optional-bin-complex.py
@@ -0,0 +1,5 @@
+# complex - is equivalent to real numbers and only used when dealing with complex arithmetics
+# bin-returns a binary of an integer
+print(bin(5)) 
+# change binary into decimal
+print(int('0b101',2))
