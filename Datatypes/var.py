960366b4e1bb70ff7b5932bee5baa230b176f8d3
DianaBeki <dianabeki54@gmail.com>
2022-05-18 08:32:24 +0300

    variables

---
+++ Datatypes/var.py
@@ -0,0 +1,20 @@
+#variables- they store information that can be used in a programm
+iq = 190
+
+print(iq)
+#best practices of writing variables
+#snake_case
+user_iq = 190
+
+#starts with lowecase or underscore
+#case sensitive
+#Dont overwrite keywords
+#letters,numbers,underscores
+
+# constants-value that dont change
+PI = 3.14
+# __TWO UNDerscores
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
