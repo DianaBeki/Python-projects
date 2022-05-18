d4fbe83668a01cf9956e5a221aa027fead5328c6
DianaBeki <dianabeki54@gmail.com>
2022-05-18 08:30:43 +0300

    numbers

---
+++ Datatypes/num.py
@@ -0,0 +1,33 @@
+# Data types
+#int
+#An integer is a number
+print(3 + 5)
+#how to check type of data
+print(type(3 + 6))
+
+float
+#a number with a decimal point
+print(type(4/6))
+
+#bool
+#str
+#list
+#tuple
+#set
+#dict
+#classes ->custom types
+#specialized data types
+
+#None - nothing
+# maths functions
+print(round(3.9)) # 4
+
+# absolute numbers means no negative value
+print(abs(-20)) # 20
+
+# Developer fundamentals - DONT READ THE DICTIONARY
+# OPERATOR Precedence/Bodmas
+print(20 - 3 * 4)
+# ** -power of
+#() brackets
+# division 
