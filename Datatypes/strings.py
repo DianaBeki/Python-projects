9d79c146f1c516d57aba1e68f2b6f59ea779c85f
DianaBeki <dianabeki54@gmail.com>
2022-05-18 08:31:42 +0300

    strings

---
+++ Datatypes/strings.py
@@ -0,0 +1,29 @@
+# strings is a piece of text written in single or double quotation
+#username = 'supercoder'
+#password = 'supersecret'
+#long_string = '''
+#WOW
+#0 0
+#---
+#'''
+#print(long_string)
+
+from site import abs_paths
+
+
+first_name = 'Diana'
+last_name = 'Beki'
+full_name = first_name + ' ' + last_name
+#print(full_name)
+
+#string concatenation means adding strings together
+#print('hello' + ' Diana')
+
+# Type conversion
+#will first convert the number into string and print it
+#print(type(str(100)))
+#print(type(int(str(100))))
+a = str(100)
+b = int(a)
+c = type(b)
+print(c)
