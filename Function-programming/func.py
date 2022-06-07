0f1b83c14d209cf3c4deaa935dd8db690cb98407
DianaBeki <dianabeki54@gmail.com>
2022-06-07 19:37:59 +0300

    add numbers

---
+++ Function-programming/func.py
@@ -0,0 +1,10 @@
+#Add your code here
+#Ask the user to enter a number
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+    
+#sum of two numbers
+addition = num1 + num2
+#printing it
+print(f"The sum of the two numbers is {(addition)}")
+    
