87b54b36ed807378750e229dda0066e310a2f624
DianaBeki <dianabeki54@gmail.com>
2022-06-11 10:32:06 +0300

    web dev

--- comb.py
+++ comb.py
@@ -1,6 +1,3 @@
-combs = []
-for x in [1,2,3]:
- for y in[6,7,8]:
-  if x!=y:
-     combs.append((x,y))
-     print(combs)
+basket = {'mango', 'apple', 'orange', 'mango', 'banana'}
+basket.duplicates()
+print(basket)
--- queue.py
+++ queue.py
@@ -1,11 +1,6 @@
-
-squares = list(map(lambda x:x**2,range(10)))
-print(squares)
-
-squares = []
-for x in range(10):
- squares.append(x**2)
- print(squares)  
-
-squares = [x**2 for x in range(10)]
-print(squares)
+combs = []
+for x in [1,2,3]:
+ for y in[6,7,8]:
+  if x!=y:
+     combs.append((x,y))
+     print(combs)
